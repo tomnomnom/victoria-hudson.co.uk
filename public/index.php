41caa2e0b23bee9947f35260eab2af4441ff0e05
Tom Hudson <mail@tomnomnom.com>
2017-02-03 23:21:56 +0000

    Adds start of index + css

--- public/index.php
+++ public/index.php
@@ -1,1 +1,20 @@
-www.victoria-hudson.co.uk
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Victoria Hudson - Actress</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+        <meta name="theme-color" content="#95130f">
+
+        <!--<link rel="icon" sizes="64x64" href="/favicon.ico" type="image/x-icon">-->
+        <link rel="canonical" href="https://www.victoria-hudson.co.uk/" />
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Raleway">
+        <link rel="stylesheet" type="text/css" href="/css/main.css"/>
+
+    </head>
+    <body>
+        <h1>Victoria Hudson</h1>
+
+    
+    </body>
+</html>
